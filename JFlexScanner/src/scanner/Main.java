6e8dde077a73f49485167a79a88d75b25d2efa0c
Gaby Garro Abdykerimov <ggarroab@gmail.com>
2016-09-30 19:54:09 -0600

    Creación del proyecto

---
+++ JFlexScanner/src/scanner/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Main.java
+ */
+package scanner;
+
+/**
+ *
+ * @author Gaby
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Obtener el archivo .jflex
+        //File file = new File(ruta);
+        // Generar el scanner
+        //Jflex.Main.generate(file);
+        // Esto genera automáticamente un .java que tiene todo lo necesario
+        // para escanear.
+        // Después de esto, hay que cargar el archivo que se escaneará
+        //Reader reader = new BufferedReader(new FileReader(ruta));
+        //Scanner scanner = new Scanner(reader);
+        // Aquí sigue: https://github.com/lavaldi/HolaCodigo/tree/master/analizadorLexico-Java/src/analizador
+    }
+    
+}
